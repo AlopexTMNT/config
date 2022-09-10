0e6d55c4f7371fb4aef93d3b02b37d6b606316b6
AlopexTMNT <74680084+AlopexTMNT@users.noreply.github.com>
2022-09-11 01:19:28 +0300

    Add files via upload
    Added a Sleep option

--- rice/.scripts/poweroptions.sh
+++ rice/.scripts/poweroptions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # dmenu theming
-lines="-l 3"
+lines="-l 4"
 font="-fn fontawesome-10"
 colors="-nb #2C323E -nf #9899a0 -sb #bbbbbb -sf #005577"
 
 first_prompt="Choose what you want to do"
-opt1="Restart\nShutdown\nHibernate"
+opt1="Restart\nShutdown\nHibernate\nSleep"
 opt2="Yes\nNo"
 
 
@@ -40,6 +40,13 @@ ask() {
 	if [[ $answer == "Yes" ]]; then
 	    systemctl hibernate
 	fi
+	exit 0
+	;;
+	"Sleep")
+	ask "$opt2" "Want to put the computer into Sleep?"
+	if [[ $answer == "Yes" ]]; then
+	    systemctl suspend
+    	fi
 	exit 0
 	;;
 esac
